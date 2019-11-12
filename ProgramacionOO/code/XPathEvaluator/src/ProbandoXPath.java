b609951f51bb8f4b40ea7113c3d3a940487cd900
Mauricio Matamala Peinado <mauricio.politecnico@gmail.com>
2019-11-12 22:12:00 +0100

    XPath modificado con ejemplos aclaratorios

--- ProgramacionOO/code/XPathEvaluator/src/ProbandoXPath.java
+++ ProgramacionOO/code/XPathEvaluator/src/ProbandoXPath.java
@@ -1,4 +1,6 @@
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.xpath.XPathExpressionException;
@@ -6,60 +8,82 @@ import javax.xml.xpath.XPathExpressionException;
 public class ProbandoXPath {
 	public static void main(String[] args) {
 		XPathEvaluator eval = new XPathEvaluator("songlist.xml");
-
 		try {
 			NodeList nodeSet = eval.evaluateXPath("/listas/lista");
-			for (int k = 0; k < nodeSet.getLength(); k++) {
-				Element e = (Element) nodeSet.item(k);
-				NodeList canciones = e.getChildNodes();
-				System.out.println("Longitud del nodeset: " + canciones.getLength());
-				for (int i = 0; i < canciones.getLength(); i++) {
-					if (canciones.item(i).getNodeType() == Element.ELEMENT_NODE) {
-						Element cancion = (Element) canciones.item(i);
-						System.out.println(cancion.getTextContent());
-						System.out.println(cancion.getAttribute("id"));
-						System.out.println(cancion.getAttribute("duracion"));
-					}
+			printNodeList(nodeSet, 0);
+
+			System.out.println("==================================================================");
+			System.out.println("                   UNA SEGUNDA ESTRATEGIA");
+			System.out.println("==================================================================");
+
+			/* NOTA:
+				En el ejemplo anterior se utilizan los métodos item() y getChildNodes() para navegar por el NodeSet.
+				Una forma alternativa de obtener los nodos de interés es mediante el método getElementsByTagName.
+				En el siguiente fragmento se imprimen las canciones de las listas.
+		    */
+
+			for (int i=0; i<nodeSet.getLength(); i++){
+				Element e = (Element) nodeSet.item(i);
+				NodeList canciones = e.getElementsByTagName("cancion");
+				for (int j=0; j<canciones.getLength(); j++){
+					System.out.println("--------------------------------");
+					printInfoNodo(canciones.item(j),0);
+					printAtributosNodo(canciones.item(j),0);
+					System.out.println("CONTENIDO: "+canciones.item(j).getTextContent());
 				}
 			}
-			/*Element e = (Element) nodeSet.item(0);
-			System.out.println(e.getAttribute(""));
-
-			NodeList canciones = e.getElementsByTagName("cancion");
-			for (int i=0; i<canciones.getLength(); i++){
-				Element cancion = (Element) canciones.item(i);
-				System.out.print("ID: " +cancion.getAttribute("id"));
-				System.out.println("DURACIÓN: " + cancion.getAttribute("duracion"));
-				System.out.println("NOMBRE: " + cancion.getTextContent());
-
-			}
-			/*NodeList canciones.item(0).getChildNodes();
-			System.out.println(canciones.item(0).hasAttributes());
-			System.out.println(canciones.item(0).getAttribute(
-			for (int i=0; i<hijosDeCancion_0.getLength(); i++){
-				System.out.println(hijosDeCancion_0.item(i).getTextContent());
-
-			}*/
-			/*ELEMENT_NODE = 1
-				ATTRIBUTE_NODE = 2
-				TEXT_NODE = 3
-			 */
-			/*for (int i=0; i<nodeSet.getLength();i++){
-				if (nodeSet.item(i).getNodeType() == Element.ELEMENT_NODE){
-					Element elemento = (Element) nodeSet.item(i);
-					System.out.println("ELEMENTO:"+elemento.getNodeName());
-					System.out.println("ATRIBUTO ID:"+elemento.getAttribute("id"));
-					System.out.println("ATRIBUTO DURACIÓN:"+elemento.getAttribute("duracion"));
-					NodeList nodosHijos = elemento.getChildNodes();
-					for (int j=0; j< nodosHijos.getLength(); j++){
-						if (nodosHijos.item(j).getNodeType() == Element.TEXT_NODE)
-							System.out.println("CONTENIDO"+nodosHijos.item(j).getTextContent());
-					}
-				}
-			}*/
 		} catch (XPathExpressionException e) {
 			e.printStackTrace();
 		}
+	}
 
+	/*
+		Acceso a los nodos dentro del NodeList utilizando los siguientes métodos:
+			- item(i) -> devuelve un Nodo
+			- getChildNodes() -> devuelve un NodeList con los nodos hijos.
+	 */
+	public static void printNodeList(NodeList listaDeNodos, int nivelProfundidad){
+		if (listaDeNodos !=  null){
+			for (int i = 0; i < listaDeNodos.getLength(); i++) {
+				System.out.println("----------------------------");
+				Node n = listaDeNodos.item(i);
+				printInfoNodo(n, nivelProfundidad);
+				printAtributosNodo(n,nivelProfundidad);
+				printNodeList(n.getChildNodes(), nivelProfundidad + 1);
+			}
+		}
+	}
+
+	private static void printInfoNodo(Node n, int nivelProfundidad){
+		System.out.println(getEspacios(nivelProfundidad) + getTipoNodo(n) + n.getNodeName());
+	}
+
+	/*
+		Dado un nodo, se obtienen sus atributos. Si hay alguno (n.getAttributes() != null) se imprime su contenido con toString()
+	 */
+	private static void printAtributosNodo(Node n, int nivelProfundidad){
+		if (n!=null){
+			NamedNodeMap atributos = n.getAttributes();
+			if (atributos != null)
+				for (int i=0; i<atributos.getLength(); i++)
+					System.out.println(getEspacios(nivelProfundidad) + "ATRIBUTO:" + atributos.item(i).toString());
+		}
+	}
+
+	private static String getTipoNodo(Node n){
+		if (n.getNodeType() == Element.ELEMENT_NODE)
+			return "NODO ELEMENTO: ";
+		else if (n.getNodeType() == Element.TEXT_NODE)
+			return "NODO TEXTO : ";
+		else if (n.getNodeType() == Element.ATTRIBUTE_NODE)
+			return "NODO ATRIBUTO: ";
+		else
+			return "NODO: ";
+	}
+
+	private static String getEspacios(int numeroEspacios){
+		String espacios = "";
+		for (int i=0; i<numeroEspacios; i++) espacios += "\t";
+		return espacios;
 	}
 }
