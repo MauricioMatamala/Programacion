3da6f85b419e0a0010bba1166363c58ba4af3254
Mauricio Matamala Peinado <mauricio.politecnico@gmail.com>
2019-11-12 13:08:21 +0100

    XPathEvaluator modificado en clase

--- ProgramacionOO/code/XPathEvaluator/src/ProbandoXPath.java
+++ ProgramacionOO/code/XPathEvaluator/src/ProbandoXPath.java
@@ -8,9 +8,42 @@ public class ProbandoXPath {
 		XPathEvaluator eval = new XPathEvaluator("songlist.xml");
 
 		try {
-			NodeList nodeSet = eval.evaluateXPath("/listas/lista[@name='Lista de TripHop']/song");
-			Element e = (Element) nodeSet.item(0);
-			Element.ATTR
+			NodeList nodeSet = eval.evaluateXPath("/listas/lista");
+			for (int k = 0; k < nodeSet.getLength(); k++) {
+				Element e = (Element) nodeSet.item(k);
+				NodeList canciones = e.getChildNodes();
+				System.out.println("Longitud del nodeset: " + canciones.getLength());
+				for (int i = 0; i < canciones.getLength(); i++) {
+					if (canciones.item(i).getNodeType() == Element.ELEMENT_NODE) {
+						Element cancion = (Element) canciones.item(i);
+						System.out.println(cancion.getTextContent());
+						System.out.println(cancion.getAttribute("id"));
+						System.out.println(cancion.getAttribute("duracion"));
+					}
+				}
+			}
+			/*Element e = (Element) nodeSet.item(0);
+			System.out.println(e.getAttribute(""));
+
+			NodeList canciones = e.getElementsByTagName("cancion");
+			for (int i=0; i<canciones.getLength(); i++){
+				Element cancion = (Element) canciones.item(i);
+				System.out.print("ID: " +cancion.getAttribute("id"));
+				System.out.println("DURACIÓN: " + cancion.getAttribute("duracion"));
+				System.out.println("NOMBRE: " + cancion.getTextContent());
+
+			}
+			/*NodeList canciones.item(0).getChildNodes();
+			System.out.println(canciones.item(0).hasAttributes());
+			System.out.println(canciones.item(0).getAttribute(
+			for (int i=0; i<hijosDeCancion_0.getLength(); i++){
+				System.out.println(hijosDeCancion_0.item(i).getTextContent());
+
+			}*/
+			/*ELEMENT_NODE = 1
+				ATTRIBUTE_NODE = 2
+				TEXT_NODE = 3
+			 */
 			/*for (int i=0; i<nodeSet.getLength();i++){
 				if (nodeSet.item(i).getNodeType() == Element.ELEMENT_NODE){
 					Element elemento = (Element) nodeSet.item(i);
